3b6fae3d9e70d54ae89215c7e45ed006fc624115
stas <stanislav.v.v@gmail.com>
2022-04-06 13:25:04 +0500

    sqlite try

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 разное за fb2 и NNN fb2 в zip
 
-  * list_fb2_in_zipfile.py -- обрабатывает .zip с .fb2 внутри, отдаёт данные в формате |-separated, поля обозначены в show_headers()
+  * `list_fb2_in_zipfile.py` -- обрабатывает `.zip` с `.fb2` внутри, отдаёт данные в формате |-separated, поля обозначены в `show_headers()` (вывод отключен)
+  * `list2sqlite.py` -- обновляет данные в базе из файлов вида `*.zip.list`, полученных при помощи `list_fb2_in_zipfile.py`
---
+++ list2sqlite.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import os
+import sys
+import codecs
+
+
+# table in database:
+"""
+CREATE TABLE "books" (
+    "zipfile"   TEXT NOT NULL,
+    "filename"  TEXT NOT NULL,
+    "genres"    TEXT,
+    "authors"   TEXT,
+    "sequence_name" TEXT,
+    "sequence_num"  TEXT,
+    "book_title"    TEXT,
+    "lang"  TEXT,
+    "annotation"    TEXT,
+    PRIMARY KEY("authors","book_title","filename")
+)
+"""
+DB = "../fb_data.sqlite"
+
+
+# quote string for sql
+def quote_identifier(s, errors="strict"):
+    encodable = s.encode("utf-8", errors).decode("utf-8")
+
+    nul_index = encodable.find("\x00")
+
+    if nul_index >= 0:
+        error = UnicodeEncodeError("NUL-terminated utf-8", encodable,
+                                   nul_index, nul_index + 1, "NUL not allowed")
+        error_handler = codecs.lookup_error(errors)
+        replacement, _ = error_handler(error)
+        encodable = encodable.replace("\x00", replacement)
+
+    return "\"" + encodable.replace("\"", "\"\"") + "\""
+
+
+# remove trailing suffix
+def rchop(s, suffix):
+    if suffix and s.endswith(suffix):
+        return s[:-len(suffix)]
+    return s
+
+
+# DEBUG:
+def listdiff(l1, l2):
+    max = len(l1)
+    if max < len(l2):
+        max = len(l2)
+    print(max, len(l1), len(l2))
+    for i in range(0, max):
+        s1 = "%02d<<<<|" % i
+        if i < len(l1):
+            s1 = s1 + l1[i]
+        s2 = "%02d>>>>|" % i
+        if i < len(l2):
+            s2 = s2 + l2[i]
+        print(s1)
+        print(s2)
+
+
+# main function
+def iterate_list(blist):
+    data = open(blist, 'r')
+    zipfile = os.path.basename(rchop(blist, '.list'))
+    con = sqlite3.connect(DB)
+    cur = con.cursor()
+    cur.execute("DELETE FROM books WHERE zipfile = ?", [zipfile])
+    con.commit()
+    while True:
+        insdata = []
+        line = data.readline()
+        # if line is empty
+        # end of file is reached
+        if not line:
+            break
+        insdatat = line.strip('\n').split('|')
+        insdata = insdatat[:9]
+        if len(insdata) != len(insdatat):
+            insdata[8] = "".join(insdatat[8:])
+            print("vvvvvvvvvvvvvvvvvvvvvvv")
+            listdiff(insdatat, insdata)
+            print("^^^^^^^^^^^^^^^^^^^^^^^")
+        cur.execute("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (insdata))
+    con.commit()
+    con.close()
+    data.close()
+
+
+if __name__ == '__main__':
+    booklist = sys.argv[1]
+    iterate_list(booklist)
