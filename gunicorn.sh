8ace563b991b8bf2cfcee7aa969da737904e6194
Stanislav Vlasov <stanislav.v.v@gmail.com>
2022-07-27 12:51:41 +0500

    update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn3 -e FLASK_ENV=prod --workers=4 'app:create_app()' --access-logfile -
+gunicorn3 -e FLASK_ENV=prod --workers=4 'app:create_app()' --access-logfile - --error-logfile -
